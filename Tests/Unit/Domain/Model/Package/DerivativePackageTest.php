6b0c4db764edba35c1975a29f3e17e6b029a8f99
Jacob Floyd <cognifloyd@gmail.com>
2013-06-27 09:35:04 -0600

    [TASK] Add empty unit tests

---
+++ Tests/Unit/Domain/Model/Package/DerivativePackageTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Cognifire\BuilderFoundation\Tests\Unit\Domain\Model\Package;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package                          *
+ * "Cognifire.BuilderFoundation".                                         *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+
+use Cognifire\BuilderFoundation\Domain\Model\Package\DerivativePackage;//Class to test
+
+/**
+ *
+ */
+class DerivativePackageTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+}
---
+++ Tests/Unit/Domain/Repository/FlowPackageRepositoryTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Cognifire\BuilderFoundation\Tests\Unit\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package                          *
+ * "Cognifire.BuilderFoundation".                                         *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+
+use Cognifire\BuilderFoundation\Domain\Repository\FlowPackageRepository;//Class to test
+
+/**
+ *
+ */
+class FlowPackageRepositoryTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+}
